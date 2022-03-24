8c2e66aaad34b432d20e324746fc4877903c66b5
Su Yatanar <suyatanar86@gmail.com>
2022-03-24 23:45:26 +0800

    Remove db username & password

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php 
 define("HOME", 'https://version.testingonline.business');
 define("DATABASE_NAME", 'suyadanar_version');
-define("DATABASE_USERNAME", 'suyadanar_version');
-define("DATABASE_PASSWORD", 'wY18s6dRX)');
+define("DATABASE_USERNAME", '');
+define("DATABASE_PASSWORD", '');
 define("DATABASE_HOSTNAME", 'localhost');
